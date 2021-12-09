c09d40fb6e6e14081bd8a7aa22f7d10aa8b68717
michal16295 <michal16292@gmail.com>
2021-12-09 11:36:32 +0200

    added a table to card page

--- src/screens/Card.js
+++ src/screens/Card.js
@@ -1,9 +1,57 @@
 import styled from "styled-components";
+import Table from "../components/Table";
 
+const CardCols = [
+  {
+    title: "type",
+    color: "mainFont",
+  },
+  {
+    title: "bank",
+    color: "mainFont",
+  },
+  {
+    title: "number",
+    color: "mainFont",
+  },
+  {
+    title: "name",
+    color: "mainFont",
+  },
+];
+const Data = [
+  {
+    type: "VISA",
+    bank: "BNI",
+    number: "4580 **** **** ****",
+    name: "Michal Barski",
+  },
+  {
+    type: "American Express",
+    bank: "BNI",
+    number: "4580 **** **** ****",
+    name: "Michal Barski",
+  },
+  {
+    type: "Master Card",
+    bank: "BNI",
+    number: "4580 **** **** ****",
+    name: "Michal Barski",
+  },
+  {
+    type: "DIRECT",
+    bank: "BNI",
+    number: "4580 **** **** ****",
+    name: "Michal Barski",
+  },
+];
 const Card = () => {
   return (
     <Container>
-      <Title>Card</Title>
+      <Title>Card Center</Title>
+      <Row>
+        <Table title="List Of Cards" cols={CardCols} data={Data} />
+      </Row>
     </Container>
   );
 };
@@ -18,4 +66,12 @@ const Title = styled.div`
   font-weight: bold;
   font-size: 36px;
   color: ${({ theme }) => theme.colors.light.mainFont};
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 15px;
+  row-gap: 20px;
+  margin: 40px 0;
 `;
